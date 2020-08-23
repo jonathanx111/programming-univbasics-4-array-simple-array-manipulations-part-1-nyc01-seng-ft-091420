372a00749c9c6a53a16fc6468de330d93163bea7
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-23 06:45:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end 
+
+def using_shift(arr)
+  arr.shift
+  
+end
+
+
